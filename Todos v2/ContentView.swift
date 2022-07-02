e3e3f0d6f0d92880e9a6843363d8c50bcae16bb8
YJ Soon <yjsoon@tinkertanker.com>
2022-07-02 11:24:46 +0800

    Added Edit button and onMove

--- Todos v2/ContentView.swift
+++ Todos v2/ContentView.swift
@@ -33,8 +33,16 @@ struct ContentView: View {
                 .onDelete { indexSet in
                     todos.remove(atOffsets: indexSet)
                 }
+                .onMove { oldOffset, newOffset in
+                    todos.move(fromOffsets: oldOffset, toOffset: newOffset)
+                }
             }
             .navigationTitle("Skill Issues")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
+            }
         }
     }
 }
