dea5b88f05e8092a88dc6a9ad487d1d4a884fb5c
YJ Soon <yjsoon@tinkertanker.com>
2022-07-02 09:11:37 +0800

    Initial Commit

---
+++ Todos v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todos v2
+//
+//  Created by YJ Soon on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Todos v2/Todos_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todos_v2App.swift
+//  Todos v2
+//
+//  Created by YJ Soon on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Todos_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
