54afd73b66261b6be966c0703e3890f48427f088
YJ Soon <yjsoon@tinkertanker.com>
2022-07-02 11:46:10 +0800

    Sheet can be dismissed now by tapping save

--- Todos v2/NewTodoView.swift
+++ Todos v2/NewTodoView.swift
@@ -12,6 +12,9 @@ struct NewTodoView: View {
     @State var todoTitle = ""
     @Binding var todos: [Todo]
     
+    @Environment(\.dismiss) var dismiss
+//    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
         Form {
             TextField("Enter todo name", text: $todoTitle)
@@ -19,6 +22,8 @@ struct NewTodoView: View {
             Button("Save todo") {
                 let todo = Todo(title: todoTitle)
                 todos.append(todo)
+                dismiss()
+//                presentationMode.wrappedValue.dismiss()
             }
         }
     }
